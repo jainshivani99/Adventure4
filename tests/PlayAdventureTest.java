9a962be774c6604a4de9951d06c0a584e971d572
Shivani Jain <jainshivani99@yahoo.com>
2018-02-06 22:13:05 -0600

    Created class PlayAdventureTest for test cases

---
+++ tests/PlayAdventureTest.java
@@ -0,0 +1,16 @@
+import com.google.gson.Gson;
+import static org.junit.Assert.*;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayAdventureTest {
+
+//    @Test
+//    public void goDirection() {
+//        assertEquals(, PlayAdventure.goDirection());
+//    }
+
+}
